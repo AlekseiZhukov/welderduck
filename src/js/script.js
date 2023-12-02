5e5ef91fdbfd3bf9ab54a75af97798fa0c5fe649
Aleksey Zhukov <alex__zhukov@mail.ru>
2023-12-02 15:07:16 +0300

    finish header and main sections

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,10 @@
 'use strict';
 
 $(document).ready( () => {
+
+    //----header color -------------------------------
+
+
     //----паралах----------------------------------
    $('.jarallax').jarallax({
         speed: 0.2,
@@ -55,6 +59,18 @@ $(document).ready( () => {
       } else {
           button.fadeOut();
       }
+      if ($(this).scrollTop() > 50 && window.screen.width <= 673) {
+          $('#logo').hide('slow');
+      } else {
+          $('#logo').show('slow');
+      }
+
+      if ($(this).scrollTop() > 50) {
+          $('.header').addClass("color");
+      } else {
+          $('.header').removeClass("color");
+      }
+
   });
   button.on('click', function(){
       $('body, html').animate({
@@ -69,6 +85,172 @@ $(document).ready( () => {
         buttonPhone.addClass('effect');
         setTimeout(()=> buttonPhone.removeClass('effect'), 1000)
     },10000)
+
+
+    //----Popup & Feedback form --------------------------------------
+    const feedbackForm = $('.feedback__form');
+    const name = $('#name');
+    const phone = $('#phone');
+    const message=$('#message');
+    const checkbox = $('#checkbox');
+    const checkboxLabel = $('#checkboxLabel');
+    const messageError = $('.message__error');
+    let hasErrorName = false;
+    let hasErrorPhone = false;
+    let hasErrorMessage = false;
+    let hasErrorCheckbox = false;
+
+    $('.show_popup').click( function () {
+        let popupId = $(`#${$(this).attr('rel')}`);
+        $(popupId).show({effect: "scale", duration: 300, complete: () => {
+                $('.popup__content').show({effect: "explode", duration: 300})
+            }});
+    });
+
+    $('.form-reset').click(resetForm);
+    function resetForm() {
+
+        $('.popup__content').hide({effect: "explode", duration: 500, complete: ()=> {
+                $('#popup1').hide({effect: "scale", duration: 500})
+            }})
+        hasErrorName = hasErrorPhone = hasErrorMessage = hasErrorCheckbox = false;
+        $('input, textarea').each( function () {
+            $(this).val('');
+        } );
+        $('.form__action').show();
+        $('.resul-message').text('').hide();
+    }
+    function validationName () {
+        if (!name.val()) {
+            name.next('.message__error').text('Введите имя');
+            name.addClass('error');
+            hasErrorName = true;
+        } else {
+            name.removeClass('error');
+            name.next('.message__error').text('');
+            hasErrorName = false
+        }
+    }
+    function validationPhone () {
+        if (!phone.val()) {
+            phone.next('.message__error').text('Введите номер телефона');
+            phone.addClass('error');
+            hasErrorPhone = true;
+        } else if (phone.val().includes('_')) {
+            phone.next('.message__error').text('Некорректный номер');
+            phone.addClass('error');
+            hasErrorPhone = true;
+        } else {
+            phone.removeClass('error');
+            phone.next('.message__error').text('');
+            hasErrorPhone = false;
+        }
+    }
+    function validationMessage () {
+        if (!message.val()) {
+            message.next('.textarea__message__error').text('Напишите сообщение');
+            message.addClass('error');
+            hasErrorMessage = true;
+        } else {
+            message.removeClass('error');
+            message.next('.textarea__message__error').text('');
+            hasErrorMessage = false;
+        }
+    }
+    function validationCheckbox () {
+        if(!checkbox[0].checked) {
+            checkboxLabel.next('.message__error').text('Подтвердите согласие');
+            checkboxLabel.addClass('error');
+            hasErrorCheckbox = true;
+        } else {
+            checkboxLabel.removeClass('error');
+            checkboxLabel.next('.message__error').text('');
+            hasErrorCheckbox = false;
+        }
+    }
+
+    //----плагин для поля номера телефона, позиционирование курсора--------
+    $.fn.setCursorPosition = function(pos) {
+        if ($(this).get(0).setSelectionRange) {
+            $(this).get(0).setSelectionRange(pos, pos);
+        } else if ($(this).get(0).createTextRange) {
+            let range = $(this).get(0).createTextRange();
+            range.collapse(true);
+            range.moveEnd('character', pos);
+            range.moveStart('character', pos);
+            range.select();
+        }
+    };
+
+    name.on('change', function () {
+        validationName()
+    })
+
+    phone.on('change', function () {
+        validationPhone()
+    })
+
+    message.on('change', function () {
+        validationMessage()
+    })
+
+    checkbox.on('change', function () {
+        validationCheckbox()
+    })
+
+    phone.click( function () {
+        $(this).setCursorPosition(3);
+    }).mask("+7(999) 999-99-99", {autoclear: false});
+
+    feedbackForm.on('submit', function(env) {
+        env.preventDefault();
+        const loader = $('.loader');
+        let hasError;
+        loader.css('display', 'flex');
+        messageError.text('');
+        validationName();
+        validationPhone();
+        validationMessage();
+        validationCheckbox();
+
+        hasError = !(!hasErrorName && !hasErrorPhone && !hasErrorMessage && !hasErrorCheckbox);
+
+        if(!hasError) {
+            const sentMessage = `Для Алексея:\n\n Имя: ${name.val()}\n\n Телефон: ${phone.val()}\n\n Сообщение: ${message.val()}`;
+            const dataForSend = {
+                chat_id: "-521382689",
+                parse_mode: "HTML",
+                text: sentMessage,
+            }
+            $.ajax({
+                method: "POST",
+                url: "https://api.telegram.org/bot1808311868:AAHC_Zz8Ju5ijZWPCVh5nAzd6Q2b6x-w-Ws/sendMessage",
+                data: dataForSend
+            })
+                .done( (msg) => {
+                    loader.hide();
+                    console.log('ajax', msg)
+                    if (msg && msg.ok) {
+                        $('.form__action').hide();
+                        $('.resul-message').text('Сообщение отправлено!').show();
+                        setTimeout( () => {
+                            resetForm('popup1')
+                        }, 3000)
+                    }
+                    if (msg && !msg.ok) {
+                        $('.form__action').hide();
+                        $('.resul-message').text('Сообщение не отправлено!').show();
+                        console.log('Сообщение не отправлено: ', msg)
+                        setTimeout( () => {
+                            resetForm('popup1')
+                        }, 3000)
+                    }
+                })
+        } else {
+            loader.hide()
+        }
+    })
+
 
 });
 
